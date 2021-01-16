cee4ba51e827100e3beeebac32ff274990743add
Tony-Stark73 <74533342+Tony-Stark73@users.noreply.github.com>
2021-01-16 23:43:46 +0530

    Add files via upload

---
+++ striker.js
@@ -0,0 +1,15 @@
+class Bird extends baseclass{
+    constructor(x, y) {
+     super(x,y,50,50)
+
+     
+    }
+   
+  display(){
+this.body.position.x=mouseX;
+this.body.position.y=mouseY;
+super.display();
+  }
+  }
+
+  
